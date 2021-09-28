aba980259c8e836d82c381446463e9fcbec9e962
RodriskoSk <91113246+RodriskoSk@users.noreply.github.com>
2021-09-28 09:18:09 +0200

    Add files via upload

---
+++ kvad rovnica.py
@@ -0,0 +1,25 @@
+import math
+
+a=int(input("Zadaj hodnotu a: "))
+b=int(input("Zadaj hodnotu b: "))
+c=int(input("Zadaj hodnotu c: "))
+
+
+d=(b*b)-(4*a*c)
+
+if d>0 :
+    d=math.sqrt(d)
+
+    x1=(-b+d)/(2*a)
+    x2=(-b-d)/(2*a)
+
+    print(x1,x2)
+
+elif d==0:
+    x1=(-b+d)/(2*a)
+
+    print(x1)
+
+elif d<0 :
+    print("kvadratická rovnica nemá koreň v reálnych číslach")
+
